19871ba68efb8510f60df4d21d0c79f8b4ad5a7d
adnankhalid46 <adnankhalid46@gmail.com>
2020-06-09 02:40:35 +0500

    message updated

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>
+          <br/>
           Updated by Adnan
-          </br>
+          
         </p>
         <a
           className="App-link"
